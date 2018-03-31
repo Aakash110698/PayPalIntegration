cf8e96a665d7c802a54879a3c7153c92e5829edd
AakashAlagarsamy <aak.aakash1998@gmail.com>
2018-03-31 12:11:42 +0530

    Initial commit

---
+++ app/src/main/java/com/aakash/paypalintegration/PayPalConfig.java
@@ -0,0 +1,11 @@
+package com.aakash.paypalintegration;
+
+/**
+ * Created by Aakash Alagarsamy on 3/30/2018.
+ */
+
+public class PayPalConfig {
+
+    public static final String PAYPAL_CLIENT_ID = "AfMxxX13vSY_gJun3pQr1eEl8SUO663CAYdIzvlLLUlhVSJ6ag3NlBoXTY54x_ndU3x_15yqnCHRAPmI";
+
+}
